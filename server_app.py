25616984ea893fa3dfbe2d459a4084fe8304a617
sjoker007 <86529813+sjoker007@users.noreply.github.com>
2021-09-27 18:59:32 +0800

    main app maybe

---
+++ server_app.py
@@ -0,0 +1,41 @@
+# * --------- IMPORTS --------- *
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from bson.json_util import dumps, loads
+import os
+from flask_pymongo import PyMongo
+import cv2
+import numpy as np
+import re
+from teachers import Teachers
+from courses import Courses
+from students import Students
+
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+# * ---------- Create App --------- *
+app = Flask(__name__)
+
+
+# * ----------MongoDB connect -------*
+app.config["MONGO_URI"] = "mongodb://localhost:27017/fras_data"
+mongo = PyMongo(app)
+
+studentCollection = mongo.db.students
+teacherCollection = mongo.db.teachers
+courseCollection = mongo.db.courses
+
+# * -----------Create routes and functions here ---------
+
+
+
+
+# To avoid cors erros
+CORS(app, support_credentials=True)
+
+
+# * -------------------- Run Server -------------------- *
+if __name__ == '__main__':
+    # * --- DEBUG MODE: --- *
+    app.run(host='127.0.0.1', port=5000, debug=True)

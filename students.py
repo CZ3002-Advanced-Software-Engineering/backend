b7bfaf98f641db16c417410c0104799ee11b73f9
sjoker007 <86529813+sjoker007@users.noreply.github.com>
2021-09-27 18:48:09 +0800

    Add files via upload

---
+++ students.py
@@ -0,0 +1,26 @@
+from bson.json_util import dumps
+from bson.json_util import ObjectId
+
+class Students():
+  def __init__(self, studentCollection):
+    self.studentCollection = studentCollection
+
+  def addNewStudent(self, obj):
+    studentId = self.studentCollection.insert(obj)
+    student = self.studentCollection.find_one({'_id': ObjectId(studentId)})
+    student = dumps(student)
+
+    return student
+
+  def getStudent(self, id):
+    student = self.studentCollection.find_one({'_id': ObjectId(id)})
+    student = dumps(student)
+    # print('student', student)
+    
+    return student
+
+  def getStudentsForParticleCourse(self, courseCode):
+    students = self.studentCollection.find({'courses': courseCode})
+    students = dumps(students)
+    
+    return students

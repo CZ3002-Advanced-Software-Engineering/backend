8d06cc0e917408fd2adfeedb365482340cdcb8fa
JaclynToh <toh_simin@hotmail.com>
2021-10-01 22:22:38 +0800

    Clean database

--- database.py
+++ database.py
@@ -6,12 +6,6 @@ import os
 from flask_pymongo import PyMongo
 from pymongo.errors import ConnectionFailure
 from pymongo import MongoClient
-# import cv2
-# import numpy as np
-# import re
-# from teachers import Teachers
-# from courses import Courses
-# from students import Students
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -19,7 +13,7 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 
 # * ----------MongoDB connect -------*
-app.config["MONGO_URI"] = "mongodb+srv://admin:p%40ssw0rd@asecluster.mgx31.mongodb.net/test"
+app.config["MONGO_URI"] = "mongodb+srv://admin:p%40ssw0rd@asecluster.mgx31.mongodb.net/database"
 mongo = PyMongo(app)
 
 # * --------MongodbCollection-------*
@@ -28,29 +22,17 @@ teacherCollection = mongo.db.teacher
 courseCollection = mongo.db.course
 attendanceListCollection = mongo.db.attendancelist
 
-# * -------MongodbConnectionTest-----*
-client = MongoClient()
-try:
-   client.admin.command('ismaster')
-except ConnectionFailure:
-   print("Server not available")
-
 # * -----------Create routes and functions here ---------
-
-
-
 @app.route("/attendance", methods=['GET'])
 def read():
-    #attendance = attendanceListCollection.find({'_id': ObjectId(id)})
-    attendance = attendanceListCollection.find_one()
-
-    print(attendance)
-    # return data
-    return jsonify(attendance)
+    attendance = attendanceListCollection.find()
+    for x in attendance:
+       print(x)
+       print("for")
+    return ('results')
 
 # To avoid cors erros
 CORS(app, support_credentials=True)
-
 
 # * -------------------- Run Server -------------------- *
 if __name__ == '__main__':

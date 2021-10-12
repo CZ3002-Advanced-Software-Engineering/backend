00e4b93055de00b623bcda51a2a6013f576cd0ad
sjoker007 <86529813+sjoker007@users.noreply.github.com>
2021-10-13 02:47:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://robinhood:password..12@frasdata.7ea7m.mongodb.net/frasdata?ssl=true&ssl_cert_reqs=CERT_NONE'
 mongo = PyMongo(app)
 
+usersCollection = mongo.db.users
+docCollection = mongo.db.docs
+
+
 @app.route('/')
 def index():
     return '''
@@ -18,14 +22,17 @@ def index():
     
     '''
 
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'document' in request.files:
         document = request.files['document']
         mongo.save_file(document.filename, document)
-        mongo.db.absent_docs.insert({'name':request.form.get('studentname'), 'date':request.form.get('date'), request.form.get'document_name': document.filename})
-        return 'Done!'
+        #document_id =mongo.db.users.insert_one({'username':request.form.get('username'), 'date': request.form.get('date'), 'document_name': document.filename}).inserted_id
+        usersCollection.insert_one({'username':request.form.get('username'), 'date': request.form.get('date'), 'document_name': document.filename})
+        doc_oid = usersCollection.find_one({'username':request.form.get('username'), 'date': request.form.get('date'), 'document_name': document.filename})['_id']
+        docCollection.insert_one({'index': ObjectId(doc_oid), 'document_name': document.filename})
+        #return dumps(document_id)
+        return "Done!"
 
 #inorder to get attachment link for download href tag must be created in the frontend section
 @app.route('/download/<studentname>/<filename>')

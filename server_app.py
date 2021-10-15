6990851beeff107a608496f9c359e5e2ca6f93e9
sjoker007 <86529813+sjoker007@users.noreply.github.com>
2021-10-16 04:23:27 +0800

    upload and insert document id into newAttendance

--- server_app.py
+++ server_app.py
@@ -37,7 +37,7 @@ mongo = PyMongo(app)
 studentCollection = mongo.db.student
 teacherCollection = mongo.db.users
 moduleCollection = mongo.db.module
-attendanceCollection = mongo.db.attendancelist
+attendanceCollection = mongo.db.newAttendance
 docCollection = mongo.db.docs
 
 #UPLOAD_FOLDER = 'C:\\Users\\USER\\Desktop\\file_upload\\backend\\uploadedfiles'
@@ -45,10 +45,6 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 # * -----------Create routes and functions here ---------
-
-      
-
-
 # login authentication for teachers and students maybe
 @app.route('/login', methods=['POST'])
 def login():
@@ -83,21 +79,21 @@ def login():
 # upload a file with respect to current database, not sure
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    name = request.args.get('name')
-    course = request.args.get('course')
-    group = request.args.get('index')
-    status = request.args.get('status')
-    date = request.args.get('date')
-
-    #resp_student = attendanceCollection.find_one({'name': name, 'course': course, 'group': group, 'date': date, 'attendance': status})
-    
+    #name = request.args.get('name')
+    #course = request.args.get('course')
+    #group = request.args.get('index')
+    #status = request.args.get('status')
+    #date = request.args.get('date')
+    student_id = request.args.get('student_id')
+    attendance_id = request.args.get('attendance_id')
+  
     if 'document' in request.files:
         document = request.files['document']
         mongo.save_file(document.filename, document)
-        docCollection.insert_one({'name': name, 'doc_name': document.filename,'date': date}) 
-        doc_oid = docCollection.find_one({'name': name, 'doc_name': document.filename, 'date': date})['_id']
+        docCollection.insert_one({'student_id': ObjectId(student_id), 'attendance_id': Object(attendance_id), 'doc_name': document.filename}) 
+        doc_oid = docCollection.find_one({'student_id': ObjectId(student_id), 'doc_name': document.filename, 'attendance_id': ObjectId(attendance_id)})['_id']
         #resp_student['documents'] = doc_oid # or ObjectId(doc_oid)???
-        attendanceCollection.find_one_and_update({'name': name, 'course': course, 'group': group, 'date': date, 'attendance': status},
+        attendanceCollection.find_one_and_update({'_id': ObjectId(attendance_id), 'student': ObjectId(student_id)},
                                                  {'$set': {'documents': ObjectId(doc_oid)}}, upsert = True)
         # update into attendancelist the id of document in mongodb part not sure
         
@@ -107,18 +103,11 @@ def upload_file():
 # direct link to download file by fileid
 @app.route('/download/<fileid>')
 def getfile(fileid):
-    #fileid = request.args.get('fileid')
     query = {'_id': ObjectId(fileid)}
     cursor = docCollection.find_one(query)
     fileName = cursor['doc_name']
-
-    #filename = docCollection.find_one({'index': ObjectId(doc_oid)})
     return mongo.send_file(fileName) #as_attachment=True#
     
-
-
-
-
 
 
 
